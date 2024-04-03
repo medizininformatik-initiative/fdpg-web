ef8221634adf3964da28f93c0c185c65a6262db3
Steven Rohner <steven_rohner@yahoo.de>
2024-04-03 10:10:12 +0200

    IONOS DB Test

--- test-connect.php
+++ test-connect.php
@@ -1,18 +1,24 @@
 <?php
 $host = getenv('DB_HOST');
-$dbname = getenv('DB_DATABASE');
 $user = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_DATABASE');
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn = new mysqli($host, $user, $password, $dbname);
 
-    $stmt = $pdo->query("SELECT * FROM `114-fdpg-test`");
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `114-test`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
         echo implode(" | ", $row) . "\n";
     }
-} catch (PDOException $e) {
-    die("Could not connect to the database $dbname :" . $e->getMessage());
+} else {
+    echo "0 results";
 }
+$conn->close();
 ?>
